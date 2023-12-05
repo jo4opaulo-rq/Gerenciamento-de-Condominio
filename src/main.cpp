44019e42d6b2d1965c13668ff5248912785e0012
Leandro Felix <leandro.almeida3029@gmail.com>
2023-12-05 15:22:54 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 int main(){
 
+    //Residentes(string nome, string cpf, string dataNascimento, int predio, int apartamento, int andar, int vagaGaragem, string dataEntrada, string dataSaida, string telefone, string email):Pessoa(nome, cpf, dataNascimento
+    Residentes r1("Joao", "123456789-10", "01/01/2000", 1, 1, 1, 1, "01/01/2020", "01/01/2021", "99999-9999", "ze@gmail.com");
+
+    cout << "Residente N°1: " << endl;
+    cout << "Nome: " << r1.getNome() << endl;
+    cout << "CPF: " << r1.getCpf() << endl;
+    cout << "Data de Nascimento: " << r1.getDataNascimento() << endl;
+    cout << "Predio: " << r1.getPredio() << endl;
+    cout << "Apartamento: " << r1.getApartamento() << endl;
+    cout << "Andar: " << r1.getAndar() << endl;
+    cout << "Vaga de Garagem: " << r1.getVagaGaragem() << endl;
+    cout << "Data de Entrada: " << r1.getDataEntrada() << endl;
+    cout << "Data de Saida: " << r1.getDataSaida() << endl;
+    cout << "Telefone: " << r1.getTelefone() << endl;
+    cout << "Email: " << r1.getEmail() << endl;
+
     //Visitante(string nomeVisitante, string nomeVisitado, int apartamento, int andar);
 
     Visitante visitante1("João", "Maria", 1, 1);
